20592a08a99cbfb78f60ec5c0133fa56ef6539e4
Ajai <ajaijeyakaran06@gmail.com>
2023-01-02 17:24:48 -0500

    Terms and conditions checkbox & input boxes looked cleaner

--- validator.js
+++ validator.js
@@ -103,6 +103,15 @@ function validation(){
         document.querySelector("#errcheck").style.display = "block"
         document.querySelector("#errcheck").innerHTML = "Passwords do not match"
     }
+
+    checked = document.querySelector("#ucheck").checked
+    document.querySelector("#errbox").style.display = "none"
+    if(checked == false){
+        f=0
+        document.querySelector("#errbox").style.display = "block"
+        document.querySelector("#errbox").innerHTML = "Please agree to the terms and conditions to continue" 
+    }   
+
     if (f==1){
         return true;
     }    
